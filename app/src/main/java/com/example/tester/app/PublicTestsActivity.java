49e361c825d9ab0b2d884b0e98d6b0ba96110c99
CEMEHEX <CEMEHEX@yandex.ru>
2015-12-27 23:37:49 +0400

    Update PublicTestsActivity.java

--- app/src/main/java/com/example/tester/app/PublicTestsActivity.java
+++ app/src/main/java/com/example/tester/app/PublicTestsActivity.java
@@ -4,8 +4,12 @@ package com.example.tester.app;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ViewSwitcher;
 
 public class PublicTestsActivity extends Activity{
@@ -22,6 +26,19 @@ public class PublicTestsActivity extends Activity{
         switcher = (ViewSwitcher) findViewById(R.id.switcher);
         progressBar = (ProgressBar) findViewById(R.id.progress);
         listView = (ListView) findViewById(R.id.listView);
+        //Список элементов в ListView
+        final String[] elements = new String[] {"1", "2", "3"/*...*/ };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,	android.R.layout.simple_list_item_1, elements);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View itemClicked, int position, long id) {
+
+            }
+        });
+
+
         progressBar.setMax(100);
         progressBar.setVisibility(View.VISIBLE);
         loader = new ListLoader(this);
